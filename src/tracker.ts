459f7de15104080fcfb220e8f961822a4df79428
34j <34j.github@proton.me>
2026-05-23 12:31:47 +0900

    feat: wip

--- src/tracker.ts
+++ src/tracker.ts
@@ -12,7 +12,7 @@ export type WaitAll<TNumber extends number, TDuration extends TNumber | undefine
 /**
  * Procedural function. useRef().current can be read and written.
  */
-export type ProceduralFunction<TNumber extends number> = () => Generator<Wait<TNumber, any, any>, void, void>
+export type ProceduralFunction<TNumber extends number> = () => Iterator<TNumber, void, void>
 /**
  * Declarative function. useRef().current is write-only.
  */
@@ -36,17 +36,19 @@ export class Tracker<TNumber extends number> {
   useRef = <T>(v: T): Ref<T> => { return { current: v } }
   sleep = (dt: TNumber): ConstantWait<TNumber> => ({ dependencies: new Set(), duration: dt, type: 'any' })
   proceduralStates: ProceduralState<TNumber>[] = []
-  proceduralDependentStates: ProceduralDependentState<TNumber>[] = []
   declarativeStates: DeclarativeState<TNumber>[] = []
   currentTime: TNumber = 0 as TNumber
   declarativeCall = (time: TNumber) => {
     // Call next() of the generator with least wait time
     while (this.proceduralStates.length > 0 && this.currentTime <= time) {
-      const nextState = this.proceduralStates.reduce((prev, curr) => prev.waitTime < curr.waitTime ? prev : curr)
-      this.proceduralStates = this.proceduralStates.filter(s => s !== nextState)
-      const nextWaitTime = nextState.waitTime
-      const iteratorResult = nextState.gen.next()
-      this.proceduralStates = this.proceduralStates.map(s => s === nextState ? { ...s, waitTime: (s.waitTime - nextWaitTime) as TNumber } : s)
+      const leastWaitState = this.proceduralStates.filter(s => s.wait.duration !== undefined).reduce((least, s) => s.wait.duration! < least.wait.duration! ? s : least)
+      if (leastWaitState.wait.duration === undefined) {
+        throw new Error('No procedural state with fixed wait time found.')
+      }
+      this.proceduralStates = this.proceduralStates.filter(s => s !== leastWaitState)
+      const nextWaitTime = leastWaitState.wait.duration!
+      const iteratorResult = leastWaitState.gen.next()
+      this.proceduralStates = this.proceduralStates.map(s => s === leastWaitState ? { ...s, waitTime: (s.wait.duration - nextWaitTime) as TNumber } : s)
 
       // If the generator is done, simply remove it
       if (iteratorResult.done) {
@@ -54,7 +56,7 @@ export class Tracker<TNumber extends number> {
       }
       // Otherwise, update the wait time of the generator to the new value returned by next()
       else {
-        this.proceduralStates.push({ gen: nextState.gen, waitTime: iteratorResult.value })
+        this.proceduralStates.push({ gen: leastWaitState.gen, wait: iteratorResult.value })
       }
     }
     this.declarativeStates.filter(s => time >= s.startTime && time < s.startTime + s.duration).forEach(s => s.fn((time - s.startTime) as TNumber))
