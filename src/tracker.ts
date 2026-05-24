9aa5868530f2cef924d1fa23a7fb6e6155e3b42a
34j <34j.github@proton.me>
2026-05-24 10:10:13 +0900

    feat: wip

--- src/tracker.ts
+++ src/tracker.ts
@@ -54,40 +54,114 @@ export interface Task<TWait extends Wait<any>>
   resume: () => void
 }
 
+/**
+ * The state of a procedural function controlled by the track.
+ */
 export interface ProceduralState<TNumber extends number> {
+  /**
+   * The procedural function.
+   */
   f: ProceduralFunction<Wait<TNumber>>
+  /**
+   * The number of `next()` calls made to the procedural function.
+   */
   totalCallsCount: number
+  /**
+   * The wait object returned by the last `next()` call to the procedural function.
+   */
   wait: Wait<TNumber>
+  /**
+   * Whether the procedural function is suspended.
+   * If suspended, the procedural function will not be called and the wait time will not be updated.
+   * When resumed, the procedural function will continue from where it was suspended.
+   */
   suspended: boolean
 }
 
 export interface DeclarativeState<TNumber extends number> {
+  /**
+   * The declarative function.
+   */
   f: DeclarativeFunction<TNumber, void>
+  /**
+   * The total time that the declarative function has been running.
+   */
   progress: TNumber
+  /**
+   * The total duration of the declarative function.
+   * Can be Infinity for infinite duration.
+   */
   duration: TNumber
+  /**
+   * Whether the declarative function is suspended.
+   * If suspended, the progress of the declarative function will not be updated and the function will not be called.
+   * When resumed, the progress will continue from where it was suspended.
+   */
   suspended: boolean
 }
 
+/**
+ * The data structure that tracks the procedural and declarative states of a system.
+ * Used to compile procedural state transitions into declarative time-to-state functions.
+ */
 export interface Track<TNumber extends number> {
+  /**
+   * The current time of the track used when compiling.
+   */
   time: TNumber
+  /**
+   * The refs used in the procedural and declarative functions.
+   * May be added to by useRef() calls.
+   */
   refs: Ref<any>[]
+  /**
+   * The procedural states of the track.
+   */
   proceduralStates: ProceduralState<TNumber>[]
+  /**
+   * The declarative states of the track.
+   */
   declarativeStates: DeclarativeState<TNumber>[]
 }
 
-export interface FixedTrack<TNumber extends number> {
+/**
+ * The materialized state of the track at a specific time.
+ */
+export interface TrackMaterialized<TNumber extends number> {
+  /**
+   * The time of the fixed track.
+   */
   time: TNumber
+  /**
+   * The values of the refs at the time of the fixed track.
+   */
   refValues: Map<Ref<any>, any>
+  /**
+   * The deepcopied procedural states of the track at the time of the fixed track.
+   */
   proceduralStates: ProceduralState<TNumber>[]
+  /**
+   * The deepcopied declarative states of the track at the time of the fixed track.
+   */
   declarativeStates: DeclarativeState<TNumber>[]
 }
 
+/**
+ * Create a ref and add it to the track.
+ * @param track The track to add the ref to.
+ * @param v The initial value of the ref.
+ * @returns The created ref.
+ */
 export function useRef<T>(track: Track<any>, v: T): Ref<T> {
   const ref = { current: v }
   track.refs.push(ref)
   return ref
 }
 
+/**
+ * Create an empty track.
+ * @returns An empty track.
+ */
 export function createTrack<TNumber extends number>(): Track<TNumber> {
   return {
     time: 0 as TNumber,
@@ -97,10 +171,22 @@ export function createTrack<TNumber extends number>(): Track<TNumber> {
   }
 }
 
+/**
+ * Sleep for a specified duration.
+ * Can be yielded by a procedural function to wait for the specified duration.
+ * @param dt The duration to sleep for.
+ * @returns A Wait object that specifies the duration to sleep for.
+ */
 export const sleep = <TNumber extends number>(dt: TNumber): Wait<TNumber> => ({ duration: dt })
 
-export function compile<TNumber extends number>(track: Track<TNumber>, time: TNumber = Infinity as TNumber): FixedTrack<TNumber>[] {
-  const fixedTracks: FixedTrack<TNumber>[] = []
+/**
+ * Compile the procedural states of the track into declarative time-to-state functions.
+ * @param track The track to compile.
+ * @param time The maximum time to compile up to. If procedural functions have side effects that are not captured by the track, it may be used instead of `useCompiled()` to get the state of the track at a specific time. If not Infinity, the declarative states will be run as well.
+ * @returns An array of fixed tracks, which are the materialized states of the track at specific times when procedural states change. Each fixed track contains the time, the values of the refs, and the procedural and declarative states at that time.
+ */
+export function compile<TNumber extends number>(track: Track<TNumber>, time: TNumber = Infinity as TNumber): TrackMaterialized<TNumber>[] {
+  const fixedTracks: TrackMaterialized<TNumber>[] = []
   track.time = 0 as TNumber
   // Call next() of the generator with least wait time
   while (track.proceduralStates.filter(s => !s.suspended).length > 0 && track.time <= time) {
@@ -171,13 +257,16 @@ export function compile<TNumber extends number>(track: Track<TNumber>, time: TNu
   return fixedTracks
 }
 
-export function useCompiled<TNumber extends number>(track: Track<TNumber>, fixedTracks: FixedTrack<TNumber>[], time: number) {
-  if (time < 0) {
-    throw new Error('Time cannot be negative.')
-  }
+/**
+ * Evaluate the state of the track at a specific time using the fixed tracks generated by `compile()`.
+ * @param track The track to evaluate. Required to update the refs.
+ * @param fixedTracks The fixed tracks generated by `compile()`.
+ * @param time The time to evaluate the track at. Must be non-negative.
+ */
+export function useCompiled<TNumber extends number>(track: Track<TNumber>, fixedTracks: TrackMaterialized<TNumber>[], time: number) {
   const fixedTrack = fixedTracks.findLast(t => t.time <= time)
   if (!fixedTrack) {
-    throw new Error('No fixed track found for the given time.')
+    throw new Error(`No fixed track found for the given time.${time < 0 ? ' (Time cannot be negative.)' : ''}`)
   }
   track.refs.forEach((ref) => {
     if (fixedTrack.refValues.has(ref)) {
@@ -187,6 +276,13 @@ export function useCompiled<TNumber extends number>(track: Track<TNumber>, fixed
   fixedTrack.declarativeStates.filter(s => s.progress < s.duration).forEach(s => s.f((s.progress + time - fixedTrack.time) as TNumber))
 }
 
+/**
+ * Run a declarative function as a task and add it to the track.
+ * @param track The track to add the task to.
+ * @param f The declarative function to run as a task. The function will be called with the progress time as the argument, and can update the refs of the track. The function will be called every time the progress time is updated until the progress time reaches the duration of the task.
+ * @param duration The duration of the task. Can be Infinity for infinite duration.
+ * @returns A Task object that can be suspended and resumed. wait() returns a Wait object that specifies the duration of the task. When the task is suspended, the progress time will not be updated and the function will not be called. When the task is resumed, the progress time will continue from where it was suspended.
+ */
 export function runDeclarative<TNumber extends number>(track: Track<TNumber>, f: DeclarativeFunction<TNumber, void>, duration: TNumber): Task<Wait<TNumber>> {
   const state = { f, progress: 0 as TNumber, duration, suspended: false }
   track.declarativeStates.push(state)
@@ -201,6 +297,12 @@ export function runDeclarative<TNumber extends number>(track: Track<TNumber>, f:
   }
 }
 
+/**
+ * Run a procedural function as a task and add it to the track.
+ * @param track The track to add the task to.
+ * @param f The procedural function to run as a task. The function will be called immediately and can update the refs of the track. The function can yield Wait objects to specify the conditions for the function to be called again. When the function is suspended, it will not be called and the wait time will not be updated. When the function is resumed, it will continue from where it was suspended.
+ * @returns A Task object that can be suspended and resumed. wait() returns a Wait object that specifies the conditions for the task to be finished. When the task is suspended, the procedural function will not be called and the wait time will not be updated. When the task is resumed, the procedural function will continue from where it was suspended.
+ */
 export function runProcedural<TNumber extends number>(track: Track<TNumber>, f: ProceduralFunction<Wait<TNumber>>): Task<Wait<TNumber>> {
   // Run immediately
   const state = { f, wait: { duration: 0 as TNumber }, totalCallsCount: 0, suspended: false }
@@ -218,6 +320,12 @@ export function runProcedural<TNumber extends number>(track: Track<TNumber>, f: 
   }
 }
 
+/**
+ * Run multiple tasks in parallel and return a task that is finished when all of the tasks are finished.
+ * @param track The track to add the task to. Required to run the task using `runProcedural()`.
+ * @param tasks The tasks to run in parallel. The returned task will be finished when all of the tasks are finished.
+ * @returns A Task object that can be suspended and resumed. wait() returns a Wait object that is finished when all of the tasks are finished. When the task is suspended, all of the tasks will not be suspended. When the task is resumed, all of the tasks will not be resumed.
+ */
 export function all<TNumber extends number>(track: Track<TNumber>, tasks: Task<Wait<TNumber>>[]): Task<Wait<TNumber>> {
   return runProcedural(
     track,
@@ -227,6 +335,11 @@ export function all<TNumber extends number>(track: Track<TNumber>, tasks: Task<W
   )
 }
 
+/**
+ * Run multiple tasks in parallel and return a task that is finished when any of the tasks is finished.
+ * @param tasks The tasks to run in parallel. The returned task will be finished when any of the tasks is finished.
+ * @returns A Task object that can be suspended and resumed. wait() returns a Wait object that is finished when any of the tasks is finished. When the task is suspended, all of the tasks will be suspended. When the task is resumed, all of the tasks will be resumed.
+ */
 export function any<TNumber extends number>(tasks: Task<Wait<TNumber>>[]): Task<Wait<TNumber>> {
   return {
     suspend: () => tasks.forEach(t => t.suspend()),
