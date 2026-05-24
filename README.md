221efa8bf9be83dd70873444c7cc9a1d66499167
34j <55338215+34j@users.noreply.github.com>
2026-05-24 13:15:56 +0900

    feat: add main feat (#1)

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<!--
+  -- This file is auto-generated from README.md. Changes should be made there.
+  -->
 
 # procedural-to-declarative
 
@@ -17,7 +20,7 @@
 
 ---
 
-TODO: Project Description.
+Compile procedural state transitions (do, wait, set, wait, ...) into declarative time-to-state functions (t -> do, set).
 
 ## Installation
 
@@ -25,14 +28,255 @@ TODO: Project Description.
 npm install procedural-to-declarative
 ```
 
-## Usage
+## Motivation
+
+Video generation using TypeScript is a hot topic. Typically such package requires a function that maps time to state.
+
+<!-- skip doccmd[all]: start -->
 
 ```ts
-import { myPackage } from 'procedural-to-declarative'
-
-myPackage('hello')
-// => 'hello from my package'
+type DeclarativeFunction<T> = (time: number) => T | undefined
 ```
+
+However, it's often more intuitive to write state transitions in a procedural way:
+
+```ts
+const x = useRef(0)
+function proc() {
+  sleep(1)
+  x.current += 1
+  sleep(1)
+  x.current += 1
+}
+```
+
+However if one would like to parallelize procudual functions, it turns out to be impossible, since the passed function cannot be "blocked" to sort the procedure (inner lines).
+
+```ts
+function proc() {
+  const x = useRef(0)
+  all([
+    (() => {
+      sleep(1)
+      x.current += 1 // 00:01 (Unable to block here!)
+      sleep(2)
+      x.current += 2 // 00:03
+    })(),
+    (() => {
+      sleep(2)
+      x.current *= 2 // 00:02
+    })(),
+  ])
+}
+```
+
+However, if we use `async/await` or `yield` (like `motion-canvas` did), we can "block" the function and sort the procedure.
+
+```ts
+async function proc() {
+  const x = useRef(0)
+  await all([
+    (() => {
+      await sleep(1) // (1)
+      x.current += 1 // 00:01
+      await sleep(2) // Blocked until (2) is executed
+      x.current += 2 // 00:03
+    })(),
+    (() => {
+      await sleep(2) // Blocked until (1) is executed (2)
+      x.current *= 2 // 00:02
+    })(),
+  ])
+}
+```
+
+```ts
+function* proc() {
+  const x = useRef(0)
+  yield* all([
+    (() => {
+      yield sleep(1) // (1)
+      x.current += 1 // 00:01
+      yield sleep(2) // Blocked until (2) is executed
+      x.current += 2 // 00:03
+    })(),
+    (() => {
+      yield sleep(2) // Blocked until (1) is executed (2)
+      x.current *= 2 // 00:02
+    })(),
+  ])
+}
+```
+
+<!-- skip doccmd[all]: end -->
+
+Our package uses the second approach.
+
+## Usage
+
+<!-- group doccmd[all]: start -->
+
+```ts
+import { plotHistory } from './plot.ts'
+import { all, any, compile, createTrack, runDeclarative, runProcedural, sleep, useCompiled, useRef } from './src/index.ts'
+```
+
+```ts
+const track = createTrack<number>()
+const x = useRef(track, 0)
+
+function* proc() {
+  yield sleep(1)
+  x.current = 1
+  yield runDeclarative(track, (time) => {
+    x.current = 1 + time
+  }, 1).wait()
+  yield sleep(1)
+  x.current += 1
+  yield sleep(1)
+}
+
+runProcedural(track, proc())
+const compiled = compile(track)
+```
+
+<!-- invisible-code-block: ts
+await plotHistory(track, compiled, x, 'plots/usage-x.png', 1000)
+-->
+
+<!-- group doccmd[all]: end -->
+
+![Usage x history](plots/usage-x.png)
+
+### Advanced Usage
+
+<!-- group doccmd[all]: start -->
+
+<!-- invisible-code-block: ts
+import { plotHistory } from './plot.ts'
+import { all, any, compile, createTrack, runDeclarative, runProcedural, sleep, useCompiled, useRef } from './src/index.ts'
+-->
+
+```ts
+const track = createTrack<number>()
+const x = useRef(track, 0)
+const y = useRef(track, 0)
+
+function* proc() {
+  const task1 = runDeclarative(track, (progress) => {
+    x.current = progress
+  }, 5)
+
+  function* task2Func() {
+    while (true) {
+      // Unfortunately this will not work as expected because declarative function is called later (x.current is always 0 here)
+      y.current += x.current
+      // This will work
+      y.current += 1
+      yield sleep(1)
+    }
+  }
+  const task2 = runProcedural(track, task2Func())
+
+  yield sleep(1)
+  task1.suspend()
+  yield sleep(1)
+  task1.resume()
+  yield task1.wait()
+  task2.suspend()
+}
+
+runProcedural(track, proc())
+const compiled = compile(track)
+```
+
+<!-- invisible-code-block: ts
+await plotHistory(track, compiled, x, 'plots/advanced-x.png', 1000)
+await plotHistory(track, compiled, y, 'plots/advanced-y.png', 1000)
+-->
+
+<!-- group doccmd[all]: end -->
+
+![Advanced x history](plots/advanced-x.png)
+![Advanced y history](plots/advanced-y.png)
+
+## Comparison
+
+<!-- skip doccmd[all]: start -->
+
+- From our observation, none of the existing libraries support "waiting" while video / audio is playing.
+- The comparison on the way of writing "animation" using static images is as follows:
+
+### Motion Canvas / Revideo
+
+```tsx
+import { Circle, makeScene2D, } from '@revideo/2d'
+import { all, createRef, makeProject, } from '@revideo/core'
+
+/**
+ * The Revideo scene
+ */
+const scene = makeScene2D('scene', function* (view) {
+  const circle = createRef<Circle>()
+  view.add(
+    <Circle
+      ref={circle}
+      fill="lightseagreen"
+    />
+  )
+  yield* all(
+    circle().width(0).width(100, 1),
+    circle().height(0).height(100, 2),
+  )
+})
+
+/**
+ * The final revideo project
+ */
+export default makeProject({
+  scenes: [scene],
+  settings: {
+    // Example settings:
+    shared: {
+      size: { x: 100, y: 100 },
+    },
+  },
+})
+```
+
+### [FrameScript](https://github.com/frame-script/FrameScript)
+
+```tsx
+import { useAnimation, useVariable } from '../src/lib/animation'
+import { BEZIER_SMOOTH } from '../src/lib/animation/functions'
+import { seconds } from '../src/lib/frame'
+import { FillFrame } from '../src/lib/layout/fill-frame'
+
+const x = useVariable(0)
+const y = useVariable(0)
+
+function scene() {
+  useAnimation(async (ctx) => {
+    await ctx.parallel([
+      ctx.move(x).to(100, seconds(1), BEZIER_SMOOTH),
+      ctx.move(y).to(100, seconds(2), BEZIER_SMOOTH)
+    ])
+  })
+
+  return (
+    <FillFrame style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <div
+        style={{
+          width: x.use(),
+          height: y.use(),
+        }}
+      />
+    </FillFrame>
+  )
+}
+```
+
+<!-- skip doccmd[all]: end -->
 
 [build-img]:https://github.com/34j/procedural-to-declarative/actions/workflows/release.yml/badge.svg
 [build-url]:https://github.com/34j/procedural-to-declarative/actions/workflows/release.yml
@@ -48,3 +292,6 @@ myPackage('hello')
 [semantic-release-url]:https://github.com/semantic-release/semantic-release
 [commitizen-img]:https://img.shields.io/badge/commitizen-friendly-brightgreen.svg
 [commitizen-url]:http://commitizen.github.io/cz-cli/
+
+---
+Generated from [README.md](README.md) by [`runmd`](https://github.com/broofa/runmd)
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,19 +1,209 @@
 import { describe, expect, it } from 'vitest'
-import { myPackage, mySubmodule } from '../src'
+import { all, any, compile, createTrack, runDeclarative, runProcedural, sleep, useCompiled, useRef } from '../src'
 
+function toVisibleFixedTracks<TNumber extends number>(fixedTracks: ReturnType<typeof compile<TNumber>>) {
+  return fixedTracks.map(fixedTrack => ({
+    ...fixedTrack,
+    refValues: Array.from(fixedTrack.refValues.entries(), ([ref, value]) => ({
+      key: {
+        current: ref.current,
+      },
+      value,
+    })),
+  }))
+}
+
+const eps = 1e-5
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello'
-      const result = myPackage(message)
-      expect(result).toMatch(message)
+  describe('tracker', () => {
+    it('should suspend and resume declarative', () => {
+      const track = createTrack()
+      const x = useRef(track, 0)
+      function* f() {
+        const task = runDeclarative(track, (time) => {
+          x.current = time
+        }, 10)
+        yield sleep(1)
+        task.suspend()
+        yield sleep(1)
+        task.resume()
+      }
+      runProcedural(track, f())
+      const fixedTracks = compile(track)
+      const visibleFixedTracks = toVisibleFixedTracks(fixedTracks)
+      expect(visibleFixedTracks).toMatchSnapshot()
+      const compiled = (time: number) => useCompiled(track, fixedTracks, time)
+      compiled(0)
+      expect(x.current).toBe(0)
+      compiled(1)
+      expect(x.current).toBe(1)
+      compiled(2)
+      expect(x.current).toBe(1)
+      compiled(3)
+      expect(x.current).toBe(2)
     })
-  })
-  describe('mySubmodule', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello'
-      const result = mySubmodule(message)
-      expect(result).toMatch(message)
+    it('should cancel and resume procedural', () => {
+      const track = createTrack()
+      const x = useRef(track, 0)
+      function* f() {
+        const taskG = runProcedural(track, g())
+        yield sleep(2)
+        taskG.suspend()
+        yield sleep(2)
+        taskG.resume()
+      }
+      function* g() {
+        yield sleep(1)
+        x.current = 1
+        yield sleep(2)
+        x.current = 2
+      }
+      runProcedural(track, f())
+      const fixedTracks = compile(track)
+      const visibleFixedTracks = toVisibleFixedTracks(fixedTracks)
+      expect(visibleFixedTracks).toMatchSnapshot()
+      const compiled = (time: number) => useCompiled(track, fixedTracks, time)
+
+      compiled(0)
+      expect(x.current).toBe(0)
+      compiled(1 - eps)
+      expect(x.current).toBe(0)
+      compiled(1)
+      expect(x.current).toBe(1)
+      compiled(5 - eps)
+      expect(x.current).toBe(1)
+      compiled(5)
+      expect(x.current).toBe(2)
+    })
+    it('should raise when dead lock', () => {
+      const track = createTrack()
+      let taskF: ReturnType<typeof runProcedural>
+      let taskG: ReturnType<typeof runProcedural>
+      function* f() {
+        yield taskG.wait()
+      }
+      function* g() {
+        yield taskF.wait()
+      }
+      taskF = runProcedural(track, f())
+      taskG = runProcedural(track, g())
+      expect(() => compile(track)).toThrow()
+    })
+    it('should correctly handle all', () => {
+      const track = createTrack()
+      const x = useRef(track, 0)
+      function* f() {
+        function* g() {
+          yield sleep(1)
+          x.current = 1
+        }
+        function* h() {
+          yield sleep(2)
+          x.current = 2
+        }
+        yield all(track, [runProcedural(track, g()), runProcedural(track, h())]).wait()
+      }
+      runProcedural(track, f())
+      const fixedTracks = compile(track)
+      const visibleFixedTracks = toVisibleFixedTracks(fixedTracks)
+      expect(visibleFixedTracks).toMatchSnapshot()
+      const compiled = (time: number) => useCompiled(track, fixedTracks, time)
+
+      compiled(0)
+      expect(x.current).toBe(0)
+      compiled(1 - eps)
+      expect(x.current).toBe(0)
+      compiled(1)
+      expect(x.current).toBe(1)
+      compiled(2 - eps)
+      expect(x.current).toBe(1)
+      compiled(2)
+      expect(x.current).toBe(2)
+      compiled(5)
+      expect(x.current).toBe(2)
+    })
+    it('should correctly handle any', () => {
+      const track = createTrack()
+      const x = useRef(track, 0)
+      function* f() {
+        function* g() {
+          yield sleep(1)
+          x.current = 1
+        }
+        function* h() {
+          yield sleep(2)
+          x.current = 2
+        }
+        yield any([runProcedural(track, g()), runProcedural(track, h())]).wait()
+        x.current = 3
+      }
+      runProcedural(track, f())
+      const fixedTracks = compile(track)
+      const visibleFixedTracks = toVisibleFixedTracks(fixedTracks)
+      expect(visibleFixedTracks).toMatchSnapshot()
+      const compiled = (time: number) => useCompiled(track, fixedTracks, time)
+
+      compiled(0)
+      expect(x.current).toBe(0)
+      compiled(1 - eps)
+      expect(x.current).toBe(0)
+      compiled(1)
+      expect(x.current).toBe(3)
+      compiled(2 - eps)
+      expect(x.current).toBe(3)
+      compiled(2)
+      expect(x.current).toBe(2)
+      compiled(5)
+      expect(x.current).toBe(2)
+    })
+
+    it('should support README example', () => {
+      const track = createTrack()
+      const x = useRef(track, 0)
+      function* f() {
+        expect(track.time).toBe(0)
+        yield sleep(1)
+        expect(track.time).toBe(1)
+        x.current = 1
+        yield sleep(1)
+        expect(track.time).toBe(2)
+        yield runDeclarative(track, (time: number) => {
+          x.current = 1 + time
+        }, 1).wait()
+        expect(track.time).toBe(3)
+        yield sleep(1)
+        expect(track.time).toBe(4)
+        x.current += 1
+        yield sleep(1)
+        expect(track.time).toBe(5)
+      }
+      runProcedural(track, f())
+      const fixedTracks = compile(track)
+      const visibleFixedTracks = toVisibleFixedTracks(fixedTracks)
+      expect(visibleFixedTracks).toMatchSnapshot()
+
+      const compiled = (time: number) => useCompiled(track, fixedTracks, time)
+      compiled(0)
+      expect(x.current).toBe(0)
+      compiled(1 - eps)
+      expect(x.current).toBe(0)
+      compiled(1)
+      expect(x.current).toBe(1)
+      compiled(2 - eps)
+      expect(x.current).toBe(1)
+      compiled(2)
+      expect(x.current).toBe(1)
+      compiled(2.5)
+      expect(x.current).toBe(1.5)
+      compiled(3)
+      expect(x.current).toBe(1)
+      compiled(4)
+      expect(x.current).toBe(2)
+      compiled(5)
+      expect(x.current).toBe(2)
+      compiled(5 + eps)
+      expect(x.current).toBe(2)
+      expect(() => compiled(-1)).toThrow()
     })
   })
 })
